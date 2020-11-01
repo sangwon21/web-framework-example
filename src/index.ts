b27099b9ff43b939c7b0d39a4538882df90eb42c
sangwon <junothyme@gmail.com>
2020-11-01 09:13:03 +0900

    feat: implemented fetch function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'name', age: 12 });
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-    console.log('hello');
-});
-
-user.trigger('change');
+user.fetch();
