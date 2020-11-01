e5b8ae54ae1620b8bc3e2673e2dfbf37ef8ac283
sangwon <junothyme@gmail.com>
2020-11-01 16:19:29 +0900

    feat: handled changes on data

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,34 @@
 import { User } from '../models/User';
 
 export class UserForm {
-    constructor(public parent: Element, public model: User) {}
+    constructor(public parent: Element, public model: User) {
+        this.bindModel();
+    }
+
+    bindModel(): void {
+        this.model.on('change', () => {
+            this.render();
+        });
+    }
 
     eventsMap(): { [key: string]: () => void } {
         return {
-            'click:button': this.onButtonClick,
+            'click:.set-age': this.onSetAgeClick,
+            'click:.set-name': this.onSetNameClick,
         };
     }
 
-    onButtonClick(): void {
-        console.log('hi there');
-    }
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input');
+
+        const name = input.value;
+
+        this.model.set({ name });
+    };
+
+    onSetAgeClick = (): void => {
+        this.model.setRandomAge();
+    };
 
     template(): string {
         return `
@@ -20,7 +37,8 @@ export class UserForm {
             <div>User name: ${this.model.get('name')}</div>
             <div>User age: ${this.model.get('age')}</div>
             <input />
-            <button>Click me</button>
+            <button class="set-name">Click Name</button>
+            <button class="set-age">Set Random Age</button>
         </div>
         `;
     }
@@ -38,6 +56,7 @@ export class UserForm {
     }
 
     render(): void {
+        this.parent.innerHTML = '';
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
 
